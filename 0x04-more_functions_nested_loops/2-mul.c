6031b91ad0f92eff0a4b04c582a9b1ab6fbf2d2a
Ashenafi Goitom <ashgoitom@gmail.com>
2023-02-23 14:34:58 -0800

    Add C program that multiplies given two number

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies given two number
+ * @a: left operand
+ * @b: right operand
+ *
+ * Return: product of given number
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
