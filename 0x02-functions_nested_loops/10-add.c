36764d31b3dd8542fd0df92968bbdd014333ec4d
Ashenafi Goitom <ashgoitom@gmail.com>
2023-02-23 00:57:46 -0800

    Add C program that adds two integer numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - add two given integer numbers
+ * @a: left operand
+ * @b: right operand
+ *
+ * Return: sum of the given numbers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
