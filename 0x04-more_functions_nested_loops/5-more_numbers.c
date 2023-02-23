5e923e15ca497b2e8c5412ec64d718f8eee3ad22
Ashenafi Goitom <ashgoitom@gmail.com>
2023-02-23 15:10:23 -0800

    Add C program that prints the numbers, fro 0-14, 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints then numbers, from 0 to 14, 10 times
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int n;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			if (n >= 10)
+				_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
