e0538e1e0cdd5b3116e3d7a2036429e96d88d979
Ashenafi Goitom <ashgoitom@gmail.com>
2023-02-27 10:28:38 -0800

    Add C program that generate random password for 101-crackme

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generate random valid passwords for the program 101-crackme
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char raw[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	int passLen = 8;
+	char newPass[9]; /* C89 does not have dynamic array length assignment */
+	int i = 0;
+	int rawLen = 0;
+
+	while (raw[rawLen] != '\0')
+		rawLen++;
+
+	srand(time(NULL));
+
+	for (i = 0; i < passLen; i++)
+	{
+		int rnd_inx = rand() % rawLen;
+
+		newPass[i] = raw[rnd_inx];
+	}
+
+	newPass[passLen] = '\0';
+
+	printf("%s", newPass);
+
+	return (0);
+}
