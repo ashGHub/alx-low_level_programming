98c09c2530603ab32c3a0ae70c92dd09242b74e9
Ashenafi Goitom <ashgoitom@gmail.com>
2023-02-27 07:17:10 -0800

    Add C program that prints elements of array to the given limit

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * print_array - Prints elements of array to the given limit
+ * @a: array
+ * @n: number of elements limit
+ *
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+			printf("%s", ", ");
+	}
+	putchar('\n');
+}
