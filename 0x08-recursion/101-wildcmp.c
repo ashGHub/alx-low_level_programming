961c13824894a3af62a9332e4f2ac95e815a225b
Ashenafi Goitom <ashgoitom@gmail.com>
2023-03-09 12:58:11 -0800

    Add C program that compares two string considering asterix

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,19 @@
+/**
+ * wildcmp - compares two strings considering asterix
+ * @s1: string
+ * @s2: string with asterix to compare too
+ *
+ * Return: 1 if equal, other-wise 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+		return (0);
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	return (0);
+}
