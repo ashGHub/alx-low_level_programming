239c44b3490c8a77b276047ea664ee293bd37429
Ashenafi Goitom <ashgoitom@gmail.com>
2023-03-20 12:50:17 -0700

    Add C program that uses struct of dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - properties for dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner name
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
