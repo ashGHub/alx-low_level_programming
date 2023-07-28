907f632f6d9a354a448822e50a055a79ce6bbfa8
Ashenafi Goitom <ashgoitom@gmail.com>
2023-07-28 03:37:24 -0700

    Fix task 5 pycodestyle for makefiles

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,6 +4,7 @@
 This is a module for island perimeter
 """
 
+
 def island_perimeter(grid):
     """finds the perimeter of an island"""
     result = 0
@@ -11,7 +12,7 @@ def island_perimeter(grid):
         for j in range(len(grid[i])):
             if grid[i][j] == 0:
                 continue
-            top = grid[i - 1][j] if i - 1 >= 0 else  0
+            top = grid[i - 1][j] if i - 1 >= 0 else 0
             left = grid[i][j - 1] if j - 1 >= 0 else 0
             result += 4
             if top == 1:
